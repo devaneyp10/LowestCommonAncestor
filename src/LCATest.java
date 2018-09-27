ceec219538c6f7e49ddb3108e66771c70142c1ad
paul <devaneyp@tcd.ie>
2018-09-27 15:11:24 +0100

    Fixed messy formatting

--- src/LCATest.java
+++ src/LCATest.java
@@ -22,25 +22,24 @@ class LCATest {
 	@Test
 	void testFindPath() {
 		LCA tree = new LCA(); 
-        tree.root = new Node(1); 
-        tree.root.left = new Node(2); 
-        tree.root.right = new Node(3); 
-        tree.root.left.left = new Node(4); 
-        tree.root.left.right = new Node(5); 
-        tree.root.right.left = new Node(6); 
-        tree.root.right.right = new Node(7); 
-        
-        assertEquals("Find LCA(4,5). Answer should be 2.",tree.findLCA(4,5),2);
-        assertEquals("Find LCA(4,6). Answer should be 1.",tree.findLCA(4,6),1);
-        assertEquals("Find LCA(3,4). Answer should be 1.",tree.findLCA(3,4),1);
-        assertEquals("Find LCA(2,4). Answer should be 2.",tree.findLCA(2,4),2);
-        assertEquals("Find LCA of absent node",tree.findLCA(8, 7),-1);
-        
-        LCA tree2 = new LCA();
-        tree.root.left = new Node(5);
-        assertEquals("Find on tree with no root.",tree.findLCA(5, 0),-1);
-        
- 
+		tree.root = new Node(1); 
+		tree.root.left = new Node(2); 
+		tree.root.right = new Node(3); 
+		tree.root.left.left = new Node(4); 
+		tree.root.left.right = new Node(5); 
+		tree.root.right.left = new Node(6); 
+		tree.root.right.right = new Node(7); 
+
+		assertEquals("Find LCA(4,5). Answer should be 2.",tree.findLCA(4,5),2);
+		assertEquals("Find LCA(4,6). Answer should be 1.",tree.findLCA(4,6),1);
+		assertEquals("Find LCA(3,4). Answer should be 1.",tree.findLCA(3,4),1);
+		assertEquals("Find LCA(2,4). Answer should be 2.",tree.findLCA(2,4),2);
+		assertEquals("Find LCA of absent node",tree.findLCA(8, 7),-1);
+
+		LCA tree2 = new LCA();
+		tree.root.left = new Node(5);
+		assertEquals("Find on tree with no root.",tree.findLCA(5, 0),-1);
+		  
 	}
 	
 	
